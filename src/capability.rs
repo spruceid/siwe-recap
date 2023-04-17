e58c50fb2c0c0a2df24ccf4044f0a7f86ea52101
chunningham <c.a.cunningham6@gmail.com>
2023-04-17 21:12:24 +1000

    use the ucan-capabilities-object crate for most things

--- src/capability.rs
+++ src/capability.rs
@@ -2,29 +2,24 @@ use crate::RESOURCE_PREFIX;
 use cid::Cid;
 use std::collections::BTreeMap;
 
-use crate::ability::{Ability, AbilityName, AbilityNamespace};
-
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use serde_with::{serde_as, DisplayFromStr};
 
 use iri_string::types::UriString;
 use siwe::Message;
 
-pub type NotaBene<T> = Vec<BTreeMap<String, T>>;
-pub type Attenuations<NB> = BTreeMap<UriString, BTreeMap<Ability, NotaBene<NB>>>;
+use ucan_capabilities_object::{
+    Ability, AbilityNameRef, AbilityNamespaceRef, Capabilities, CapsInner, ConvertError,
+    NotaBeneCollection,
+};
 
 /// Representation of a set of delegated Capabilities.
 #[serde_as]
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Capability<NB = Value>
-where
-    NB: for<'d> Deserialize<'d> + Serialize,
-{
+pub struct Capability<NB> {
     /// The actions that are allowed for the given target within this namespace.
     #[serde(rename = "att")]
-    #[serde_as(as = "BTreeMap<DisplayFromStr, _>")]
-    attenuations: BTreeMap<UriString, BTreeMap<Ability, NotaBene<NB>>>,
+    attenuations: Capabilities<NB>,
 
     /// Cids of parent delegations which these capabilities are attenuated from
     #[serde(rename = "prf")]
@@ -32,22 +27,11 @@ where
     proof: Vec<Cid>,
 }
 
-#[derive(thiserror::Error, Debug)]
-pub enum ConvertError<T, A> {
-    #[error("Invalid Target: {0}")]
-    InvalidTarget(T),
-    #[error("Invalid Action: {0}")]
-    InvalidAction(A),
-}
-
-impl<NB> Capability<NB>
-where
-    NB: for<'d> Deserialize<'d> + Serialize,
-{
+impl<NB> Capability<NB> {
     /// Create a new empty Capability.
     pub fn new() -> Self {
         Self {
-            attenuations: Default::default(),
+            attenuations: Capabilities::new(),
             proof: Default::default(),
         }
     }
@@ -57,15 +41,12 @@ where
         &self,
         target: T,
         action: A,
-    ) -> Result<Option<impl Iterator<Item = &BTreeMap<String, NB>>>, ConvertError<T::Error, A::Error>>
+    ) -> Result<Option<&NotaBeneCollection<NB>>, ConvertError<T::Error, A::Error>>
     where
         T: TryInto<UriString>,
         A: TryInto<Ability>,
     {
-        Ok(self.can_do(
-            &target.try_into().map_err(ConvertError::InvalidTarget)?,
-            &action.try_into().map_err(ConvertError::InvalidAction)?,
-        ))
+        self.attenuations.can(target, action)
     }
 
     /// Check if a particular action is allowed for the specified target, or is allowed globally, without type conversion.
@@ -73,44 +54,37 @@ where
         &'l self,
         target: &UriString,
         action: &Ability,
-    ) -> Option<impl Iterator<Item = &'l BTreeMap<String, NB>>> {
-        self.attenuations
-            .get(target)
-            .and_then(|m| m.get(action))
-            .map(|v| v.iter())
+    ) -> Option<&NotaBeneCollection<NB>> {
+        self.attenuations.can_do(target, action)
     }
 
     /// Merge this Capabilities set with another
-    pub fn merge(mut self, other: Self) -> Self {
+    pub fn merge<NB1, NB2>(self, other: Capability<NB1>) -> Capability<NB2>
+    where
+        NB2: From<NB> + From<NB1>,
+    {
+        let (caps, mut proofs) = self.into_inner();
         for proof in &other.proof {
-            if self.proof.contains(proof) {
+            if proofs.contains(proof) {
                 continue;
             }
-            self.proof.push(*proof);
+            proofs.push(*proof);
         }
 
-        for (uri, abs) in other.attenuations.into_iter() {
-            let res_entry = self.attenuations.entry(uri).or_default();
-            for (ab, nbs) in abs.into_iter() {
-                res_entry.entry(ab).or_default().extend(nbs);
-            }
+        Capability {
+            attenuations: caps.merge(other.attenuations),
+            proof: proofs,
         }
-        self
     }
 
     /// Add an allowed action for the given target, with a set of note-benes
     pub fn with_action(
-        mut self,
+        &mut self,
         target: UriString,
         action: Ability,
         nb: impl IntoIterator<Item = BTreeMap<String, NB>>,
-    ) -> Self {
-        self.attenuations
-            .entry(target)
-            .or_default()
-            .entry(action)
-            .or_default()
-            .extend(nb);
+    ) -> &mut Self {
+        self.attenuations.with_action(target, action, nb);
         self
     }
 
@@ -118,33 +92,26 @@ where
     ///
     /// This method automatically converts the provided args into the correct types for convenience.
     pub fn with_action_convert<T, A>(
-        self,
+        &mut self,
         target: T,
         action: A,
         nb: impl IntoIterator<Item = BTreeMap<String, NB>>,
-    ) -> Result<Self, ConvertError<T::Error, A::Error>>
+    ) -> Result<&mut Self, ConvertError<T::Error, A::Error>>
     where
         T: TryInto<UriString>,
         A: TryInto<Ability>,
     {
-        Ok(self.with_action(
-            target.try_into().map_err(ConvertError::InvalidTarget)?,
-            action.try_into().map_err(ConvertError::InvalidAction)?,
-            nb,
-        ))
+        self.attenuations.with_action_convert(target, action, nb)?;
+        Ok(self)
     }
 
     /// Add a set of allowed action for the given target, with associated note-benes
     pub fn with_actions(
-        mut self,
+        &mut self,
         target: UriString,
         abilities: impl IntoIterator<Item = (Ability, impl IntoIterator<Item = BTreeMap<String, NB>>)>,
-    ) -> Self {
-        let entry = self.attenuations.entry(target).or_default();
-        for (ability, nbs) in abilities {
-            let ab_entry = entry.entry(ability).or_default();
-            ab_entry.extend(nbs);
-        }
+    ) -> &mut Self {
+        self.attenuations.with_actions(target, abilities);
         self
     }
 
@@ -152,39 +119,33 @@ where
     ///
     /// This method automatically converts the provided args into the correct types for convenience.
     pub fn with_actions_convert<T, A, N>(
-        self,
+        &mut self,
         target: T,
         abilities: impl IntoIterator<Item = (A, N)>,
-    ) -> Result<Self, ConvertError<T::Error, A::Error>>
+    ) -> Result<&mut Self, ConvertError<T::Error, A::Error>>
     where
         T: TryInto<UriString>,
         A: TryInto<Ability>,
         N: IntoIterator<Item = BTreeMap<String, NB>>,
     {
-        Ok(self.with_actions(
-            target.try_into().map_err(ConvertError::InvalidTarget)?,
-            abilities
-                .into_iter()
-                .map(|(a, n)| Ok((a.try_into()?, n)))
-                .collect::<Result<Vec<(Ability, N)>, A::Error>>()
-                .map_err(ConvertError::InvalidAction)?,
-        ))
+        self.attenuations.with_actions_convert(target, abilities)?;
+        Ok(self)
     }
 
     /// Read the set of abilities granted in this capabilities set
-    pub fn abilities(&self) -> &BTreeMap<UriString, BTreeMap<Ability, NotaBene<NB>>> {
-        &self.attenuations
+    pub fn abilities(&self) -> &CapsInner<NB> {
+        self.attenuations.abilities()
     }
 
     /// Read the set of abilities granted for a given target in this capabilities set
     pub fn abilities_for<T>(
         &self,
         target: T,
-    ) -> Result<Option<&BTreeMap<Ability, NotaBene<NB>>>, T::Error>
+    ) -> Result<Option<&BTreeMap<Ability, NotaBeneCollection<NB>>>, T::Error>
     where
         T: TryInto<UriString>,
     {
-        Ok(self.attenuations.get(&target.try_into()?))
+        self.attenuations.abilities_for(target)
     }
 
     /// Read the set of proofs which support the granted capabilities
@@ -214,23 +175,26 @@ where
 
     fn to_line_groups(
         &self,
-    ) -> impl Iterator<Item = (&UriString, &AbilityNamespace, Vec<&AbilityName>)> {
-        self.attenuations.iter().flat_map(|(resource, abilities)| {
-            // group abilities by namespace
-            abilities
-                .iter()
-                .fold(
-                    BTreeMap::<&AbilityNamespace, Vec<&AbilityName>>::new(),
-                    |mut map, (ability, _)| {
-                        map.entry(ability.namespace())
-                            .or_default()
-                            .push(ability.name());
-                        map
-                    },
-                )
-                .into_iter()
-                .map(move |(namespace, names)| (resource, namespace, names))
-        })
+    ) -> impl Iterator<Item = (&UriString, AbilityNamespaceRef, Vec<AbilityNameRef>)> {
+        self.attenuations
+            .abilities()
+            .iter()
+            .flat_map(|(resource, abilities)| {
+                // group abilities by namespace
+                abilities
+                    .iter()
+                    .fold(
+                        BTreeMap::<AbilityNamespaceRef, Vec<AbilityNameRef>>::new(),
+                        |mut map, (ability, _)| {
+                            map.entry(ability.namespace())
+                                .or_default()
+                                .push(ability.name());
+                            map
+                        },
+                    )
+                    .into_iter()
+                    .map(move |(namespace, names)| (resource, namespace, names))
+            })
     }
 
     fn to_statement_lines(&self) -> impl Iterator<Item = String> + '_ {
@@ -248,13 +212,36 @@ where
         })
     }
 
-    pub fn into_inner(self) -> (Attenuations<NB>, Vec<Cid>) {
+    pub fn into_inner(self) -> (Capabilities<NB>, Vec<Cid>) {
         (self.attenuations, self.proof)
+    }
+    /// Generate a ReCap statement from capabilities and URI (delegee).
+    pub fn to_statement(&self) -> String {
+        [
+            "I further authorize the stated URI to perform the following actions on my behalf:"
+                .to_string(),
+            self.to_statement_lines()
+                .enumerate()
+                .map(|(n, line)| format!(" ({}) {line}", n + 1))
+                .collect(),
+        ]
+        .concat()
+    }
+}
+
+impl<NB> Capability<NB>
+where
+    NB: Serialize,
+{
+    fn encode(&self) -> Result<String, EncodingError> {
+        serde_jcs::to_vec(self)
+            .map_err(EncodingError::Ser)
+            .map(|bytes| base64::encode_config(bytes, base64::URL_SAFE_NO_PAD))
     }
 
     /// Apply this capabilities set to a SIWE message by writing to it's statement and resource list
     pub fn build_message(&self, mut message: Message) -> Result<Message, EncodingError> {
-        if self.attenuations.is_empty() {
+        if self.attenuations.abilities().is_empty() {
             return Ok(message);
         }
         let statement = self.to_statement();
@@ -268,30 +255,12 @@ where
         });
         Ok(message)
     }
+}
 
-    /// Generate a ReCap statement from capabilities and URI (delegee).
-    pub fn to_statement(&self) -> String {
-        [
-            "I further authorize the stated URI to perform the following actions on my behalf:"
-                .to_string(),
-            self.to_statement_lines()
-                .enumerate()
-                .map(|(n, line)| format!(" ({}) {line}", n + 1))
-                .collect(),
-        ]
-        .concat()
-    }
-
-    fn extract(message: &Message) -> Result<Option<Self>, DecodingError> {
-        message
-            .resources
-            .iter()
-            .last()
-            .filter(|u| u.as_str().starts_with(RESOURCE_PREFIX))
-            .map(Self::try_from)
-            .transpose()
-    }
-
+impl<NB> Capability<NB>
+where
+    NB: for<'a> Deserialize<'a>,
+{
     /// Extract the encoded capabilities from a SIWE message and ensures the correctness of the statement.
     pub fn extract_and_verify(message: &Message) -> Result<Option<Self>, VerificationError> {
         if let Some(c) = Self::extract(message)? {
@@ -306,10 +275,14 @@ where
         }
     }
 
-    fn encode(&self) -> Result<String, EncodingError> {
-        serde_json::to_vec(self)
-            .map_err(EncodingError::Ser)
-            .map(|bytes| base64::encode_config(bytes, base64::URL_SAFE_NO_PAD))
+    fn extract(message: &Message) -> Result<Option<Self>, DecodingError> {
+        message
+            .resources
+            .iter()
+            .last()
+            .filter(|u| u.as_str().starts_with(RESOURCE_PREFIX))
+            .map(Self::try_from)
+            .transpose()
     }
 
     fn decode(encoded: &str) -> Result<Self, DecodingError> {
@@ -319,10 +292,7 @@ where
     }
 }
 
-impl<NB> Default for Capability<NB>
-where
-    NB: for<'d> Deserialize<'d> + Serialize,
-{
+impl<NB> Default for Capability<NB> {
     fn default() -> Self {
         Self::new()
     }
@@ -330,7 +300,7 @@ where
 
 impl<NB> TryFrom<&UriString> for Capability<NB>
 where
-    NB: for<'d> Deserialize<'d> + Serialize,
+    NB: for<'a> Deserialize<'a>,
 {
     type Error = DecodingError;
     fn try_from(uri: &UriString) -> Result<Self, Self::Error> {
@@ -343,7 +313,7 @@ where
 
 impl<NB> TryFrom<&Capability<NB>> for UriString
 where
-    NB: for<'d> Deserialize<'d> + Serialize,
+    NB: Serialize,
 {
     type Error = EncodingError;
     fn try_from(cap: &Capability<NB>) -> Result<Self, Self::Error> {
@@ -390,8 +360,8 @@ mod test {
 
     #[test]
     fn deser() {
-        let cap: Capability = serde_json::from_str(JSON_CAP).unwrap();
-        let reser = serde_json::to_string(&cap).unwrap();
+        let cap: Capability<serde_json::Value> = serde_json::from_str(JSON_CAP).unwrap();
+        let reser = serde_jcs::to_string(&cap).unwrap();
         assert_eq!(JSON_CAP.trim(), reser);
     }
 }
